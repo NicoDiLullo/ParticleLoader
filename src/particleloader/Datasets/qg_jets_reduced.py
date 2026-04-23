8c7a562de7b398b55f7552e7bc159385f51c0eea
NicoDiLullo <ndilullo@thacher.org>
2026-04-23 18:30:39 -0400

    sketch out qg reduced data loader

---
+++ src/particleloader/Datasets/qg_jets_reduced.py
@@ -0,0 +1,16 @@
+from particleloader.Datasets.Dataset import Dataset
+
+ZENODO_URLS = {
+    #TODO
+}
+
+hashes = {
+    #TODO
+}
+qg_jets_reduced = Dataset(
+    #TODO
+)
+
+
+qg_jets_reduced.add_url('zenodo', ZENODO_URLS)
+qg_jets_reduced.add_hash(hashes)
