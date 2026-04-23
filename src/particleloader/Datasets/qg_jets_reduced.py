f8fdd4bfb3ddf2da29430ff2fb65dae759fa7213
NicoDiLullo <ndilullo@thacher.org>
2026-04-23 19:18:38 -0400

    hashes for fp32

--- src/particleloader/Datasets/qg_jets_reduced.py
+++ src/particleloader/Datasets/qg_jets_reduced.py
@@ -37,11 +37,32 @@ ZENODO_URLS = {
 
 
 hashes = {
-    '[dtype:fp32]': [#TODO
-            ],
+    '[dtype:fp32]': [
+        'fee5d383156c6ae5d16ea9f89dbf5a93',
+        'c8729c8b0f4e614cde025618f97499cc',
+        'c7aed82434530bfddfaf21bd5bc3bb21',
+        '9dfdc199308960ff86a6836c8b56b4eb',
+        '2fad5e83be2dc882081ed20c1326bdf4',
+        '8b49219eda780e527cc8c26e8db99e82',
+        '8b4edaf0d51822786292a86181a156fe',
+        '038d87a2c9a5c21d9b81d39f0ab902bc',
+        '99229ec71d731f073d234ca4615d186d',
+        '104bca58cb34d01e73616f4a13a295c2',
+        '29b4d62df4bfef61db41fae34a6e3d09',
+        '819ed408db257c5b9c9a0d6e4342dd3a',
+        'b32427d0941c37f8a5dc0ea2eae440a4',
+        '3dc51f84007970b7a55326a7e974f0cb',
+        '4dc2a08e048eb062a0bb2a18f80b088d',
+        '8006c3580220dee876583b3210ed7e90',
+        '2b31d72b51f1f2ad1d3ee09fadf18b52',
+        'b6300b5b2e3fea39e932035ac7ba9d36',
+        'a62edf7f14e9377b496c3d4ad0411657',
+        'be969642ab8435c3658af15a3f88fa7e',
+        ],
 
     '[dtype:fp16]': [#TODO
-            ],
+        
+        ],
 
     '[dtype:fp8e4m3fn]': [#TODO
         ],
