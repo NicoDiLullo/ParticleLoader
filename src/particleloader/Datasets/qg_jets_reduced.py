7196145374dcf7dd84f926f8ecc9283490c8b46f
NicoDiLullo <ndilullo@thacher.org>
2026-04-23 19:10:19 -0400

    Better sketch of needed URLS and hashes

--- src/particleloader/Datasets/qg_jets_reduced.py
+++ src/particleloader/Datasets/qg_jets_reduced.py
@@ -1,16 +1,43 @@
 from particleloader.Datasets.Dataset import Dataset
 
+
 ZENODO_URLS = {
-    #TODO
+    '[dtype:fp32]': [
+        #TODO
+    ],
+
+    '[dtype:fp16]': [ #TODO
+
+
+    ],
+
+    '[dtype:fp8e4m3fn]': [ #TODO
+
+    ],
 }
+
 
 hashes = {
-    #TODO
-}
-qg_jets_reduced = Dataset(
-    #TODO
-)
+    '[dtype:fp32]': [#TODO
+            ],
 
+    '[dtype:fp16]': [#TODO
+            ],
+
+    '[dtype:fp8e4m3fn]': [#TODO
+        ],
+}
+
+qg_jets_reduced = Dataset(
+    name = 'qg_jets_reduced',
+    description = 'Quark and Gluon jets (reduced precision: fp32, fp16, fp8e4m3fn)',
+    NUM_PER_FILE = 100000,
+    NUM_FILES = 20,
+    options = {"dtype": ["fp32", "fp16", "fp8e4m3fn"]},
+    sources = ['zenodo'],
+    shapes = [(150, 4), (1,)],
+    npz_keys=['X', 'y'],
+)
 
 qg_jets_reduced.add_url('zenodo', ZENODO_URLS)
 qg_jets_reduced.add_hash(hashes)
